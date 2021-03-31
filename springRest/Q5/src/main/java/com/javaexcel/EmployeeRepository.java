c212e610c397dc3211ab8db0a0dd671883a9116d
Sourabh789ss <sourabh789.ss@gmail.com>
2021-03-31 11:17:11 +0530

    Spring Rest Assignments

---
+++ springRest/Q5/src/main/java/com/javaexcel/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.javaexcel;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
---
+++ springRest/Q5/src/main/java/com/javaexcel/EmployeeSarter.java
@@ -0,0 +1,13 @@
+package com.javaexcel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeStarter.class, args);
+
+    }
+
+}
